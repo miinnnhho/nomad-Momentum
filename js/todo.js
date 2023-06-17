ed9b8e7b423a39c3320a1e8114f7548533acba06
minho <jomin981007@naver.com>
2023-06-17 22:45:55 +0900

    delete toDo

--- js/todo.js
+++ js/todo.js
@@ -1,10 +1,11 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("#todo-form input");
-const toDoList = document.getElementById("todo-list");
+const toDoList = document.getElementById("todo-toDolist");
 
 const TODOS_KEY = "todos";
 
 let toDos = [];
+//database에 todo 내용 저장
 //[]로 비어있으면 초기화가 됨(덮어씀)-> const를 let으로 바꿔서 업데이트가 가능하게 변경.
 
 //todos 넣으면 저장
@@ -13,20 +14,22 @@ function saveToDos() {
 }
 
 //todo 삭제 함수
-function deleteToDO(event) {
+function deleteToDo(event) {
   const li = event.target.parentElement; //뭘 지웠는지 봐야해서 사용
   li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== li.id);
 }
 
 //li, span을 만들고 텍스트를 입력하면 span에 들어가게 만들어줌.
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   //li 만들어줄거임
+  li.id = newTodo.id;
   const span = document.createElement("span");
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   const button = document.createElement("button");
   button.innerText = "❌";
-  button.addEventListener("click", deleteToDO);
+  button.addEventListener("click", deleteToDo);
 
   li.appendChild(span); //span을 li에 넣어줌
   li.appendChild(button); //button을 li에 넣어줌
@@ -37,8 +40,12 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
-  toDos.push(newTodo);
-  paintToDo(newTodo);
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj); //user.text -> database (푸쉬함);
+  paintToDo(newTodoObj);
   saveToDos();
 }
 
