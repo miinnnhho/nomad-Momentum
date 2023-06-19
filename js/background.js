e65ffcb0290e7d8161ee422ee9bdc9410d7b4bf3
minho <jomin981007@naver.com>
2023-06-19 11:27:49 +0900

    기온표시방식  수정

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-//배경화면 랜덤 변경
+// // 배경화면 랜덤 변경
 // const images = ["0.jpg", "1.jpg", "2.jpg"];
 // const chosenImage = images[Math.floor(Math.random() * images.length)]; //사진 랜덤으로 보여줌.
 // const bgImage = document.createElement("img");
--- js/weather.js
+++ js/weather.js
@@ -5,13 +5,16 @@ function onGeoOk(position) {
   const lon = position.coords.longitude;
   //현재 내 위치를 알려줌
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       const weather = document.querySelector("#weather li:first-child");
       const city = document.querySelector("#weather li:last-child");
+      const weatherNumber = Math.floor(data.main.temp); //기온 소수점 제거하여 간단하게 표현
+
       city.innerText = data.name;
-      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`; //표현
+      weather.innerText = `${data.weather[0].main} / ${weatherNumber}`; //표현
     });
 }
 function onGeoError() {
